d0ffaa790b4d173e5b8af4288a934ead16355995
David Vandenbrouck <david_vdb@live.com>
2024-07-01 13:18:20 +0200

    Initial files and structure

--- lem-in.h
+++ lem-in.h
@@ -1,4 +1,36 @@
 #ifndef LEMIN_H
-# define LEMIN_H
+#define LEMIN_H
+#include "libft.h"
+#include <stdio.h>
+
+typedef unsigned int uint;
+
+typedef struct s_room
+{
+    uint x;
+    uint y;
+    char *name;
+    struct s_room **edges;
+} t_room;
+
+typedef struct s_path
+{
+    uint size;
+    uint max_size;
+    t_room **rooms;
+} t_path;
+
+typedef struct s_data
+{
+    t_room *start;
+    t_room *end;
+    uint width;
+    uint num_rooms;
+    t_room **temp_rooms;
+    uint hash_table_size;
+    t_room **rooms;
+    t_path **partial_paths;
+    t_path **full_paths;
+} t_data;
 
 #endif
--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 
-#include <stdio.h>
 #include "get_next_line.h"
-#include "libft.h"
 
 int main()
 {
@@ -14,3 +12,31 @@ int main()
     }
     return 0;
 }
+
+
+// Path limiters
+// number of edges to start and end
+// number of ants
+// intersections
+
+
+
+// Path eliminators
+
+// during path building
+// visiting already visited nodes for this path
+
+// after path buliding
+// 2 path with a single intersection overall with each other, shorter path wins
+
+
+
+// Path algo
+
+// find shortest path(s) of length n
+
+// will number (current width) of paths of length (n+1) be helpful?
+
+
+
+// special case: direct line from start to end
