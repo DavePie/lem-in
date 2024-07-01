85c2c8439f8e8ba73ea2ca3f15e34f79cb47a28f
David Vandenbrouck <david_vdb@live.com>
2024-07-01 15:17:10 +0200

    added extend_path logic

--- algo.c
+++ algo.c
@@ -17,12 +17,10 @@ t_path **find_best_paths(t_data *data)
     initialize_paths(data);
     while (layer_size && !best_paths(data, best_paths_arr, n_new_paths, depth))
     {
-        t_path *cur_path = data->partial_paths[0];
-        while (cur_path)
-        {
-            
-            cur_path++;
-        }
+        uint n = data->n_partial_paths;
+        for (int path_i = 0; path_i < n; path_i++)
+            extend_path(data, path_i, &n);
+
         depth++;
     }
 
@@ -57,6 +55,15 @@ void initialize_paths(t_data *data)
     }
 }
 
+// make sure to not operate on new paths
+void extend_path(t_data *data, uint index, uint *n)
+{
+    // cases:
+    // remove path, replace with non extended path (n = num path) (n--, num path --)
+    // remove path, replace with extended path (num path --)
+    // add path, excess goes at end (num path += ?)
+}
+
 uint path_contains(t_path *path, t_room *room)
 {
     for (int i = 0; i < path->size; i++)
@@ -75,10 +82,11 @@ uint best_paths(t_data *data, t_path **paths, uint n_new_paths, uint depth)
     {
 
     }
-    if (!better_combination(data, depth))
+    return better_combination(data, depth) > n_turns;
+    
 }
 
 uint better_combination(t_data *data, uint depth)
 {
-
+    return 0;
 }
