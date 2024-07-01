50b18441baf8aa8c65c75816df0903e0eced8831
alde-oli <alxunit01@gmail.com>
2024-07-01 15:53:35 +0200

    ft_realloc done

---
+++ libft/ft_realloc.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "libft.h"
+
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+	void	*new_ptr;
+
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+	ft_bzero(new_ptr, new_size);
+	if (ptr)
+	{
+		size_t min_size = old_size < new_size ? old_size : new_size;
+		ft_memcpy(new_ptr, ptr, min_size);
+		free(ptr);
+	}
+	return (new_ptr);
+}
